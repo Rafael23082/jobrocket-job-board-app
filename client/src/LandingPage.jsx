c09ada2b5dd09427b815c75a6c14bae7b28582d7
Rafael23082 <raf.anderson777@gmail.com>
2025-07-28 11:52:52 +0700

    Updated landing page

--- client/src/LandingPage.jsx
+++ client/src/LandingPage.jsx
@@ -1,19 +1,31 @@
 import Navbar from "./components/Navbar.jsx";
+import HeroSection from "./components/HeroSection.jsx";
 
 function LandingPage(){
   return(
-    <>
-      <Navbar current={"Overview"} />  
-      <div className="w-[100%] overflow-hidden bg-cover bg-center text-center relative py-[11em]" style={{backgroundImage: "url(work.jpg)", fontFamily: "'Roboto', sans-serif"}}>
-        <div className="absolute inset-0 z-0" style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}></div>
-        <p className="text-white relative z-10 text-[2.5rem] font-medium" style={{fontFamily: "'Raleway', sans-serif"}}>Work with purpose, from anywhere</p>
-        <p className="text-white relative z-10 mt-[1em] font-medium text-[1.3rem]">Find your next remote opportunity in tech, design, marketing, and more.</p>
-        <button className="px-[3.5em] py-[1.3em] bg-[#3B82F6] text-white cursor-pointer hover:bg-blue-600 transition ease duration-[0.3s] relative z-10 mt-[3em] font-semibold rounded-[5px]">BROWSE JOBS</button>
-        <div className="flex mt-[4em] justify-center relative z-10 items-center">
-          <p className="text-white font-medium text-[1.15rem]">Are you hiring remote talent?</p>
-          <button className="ml-[1em] border border-white border-[2px] text-white px-[1.4em] py-[0.6em] rounded-[10px] text-[0.88rem] font-semibold mr-[1em] cursor-pointer hover:bg-blue-50 hover:text-blue-600 transition ease duration-[0.3s]">POST A JOB</button>
-        </div>
-      </div>
+    <> 
+      <header className="mx-auto max-w-[1300px]">
+        <Navbar current={"Overview"} />  
+      </header>
+
+      <section>
+        <HeroSection />
+      </section>
+
+      <section className="flex flex-col lg:flex-row justify-around font-bold bg-[#3B82F6] text-white py-[7em] text-[1rem] lg:text-[1.5rem]" style={{fontFamily: "'Roboto', sans-serif"}}>
+        <span className="text-center leading-[3em] leading">
+          <p className="number text-[6rem]">1M+</p>
+          <p>Jobs Secured</p>
+        </span>
+        <span className="text-center leading-[3em] pt-[2.5em] lg:pt-0 leading">
+          <p className="number text-[6rem]">100K+</p>
+          <p>Tech Jobs</p>
+        </span>
+        <span className="text-center leading-[3em] pt-[2.5em] lg:pt-0 leading">
+          <p className="number text-[6rem]">4M+</p>
+          <p>Workers</p>
+        </span>
+      </section>
     </>
   );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
+import { IoMdMenu } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
+
 function Navbar({current}){
+    const [menuOpen, setMenuOpen] = useState(false);
     return(
         <>
-            <div className="flex font-medium justify-between items-center px-[3em] py-[1em]" style={{fontFamily: "'Roboto', sans-serif"}}>
-                <p className="text-[2rem] font-bold" style={{fontFamily: "'Raleway', sans-serif"}}>JobRocket</p>
-                <div className="flex text-[0.9rem]">
+            <div className="flex w-[100%] box-border font-medium justify-between items-center py-[1em] relative" style={{fontFamily: "'Roboto', sans-serif"}}>
+                <p className="text-[2rem] font-bold text-[#3B82F6] pl-10 select-none" style={{fontFamily: "'Raleway', sans-serif"}}>JobRocket</p>
+                <div className="text-[0.9rem] hidden lg:flex">
                     <p className={`cursor-pointer ${current == "Overview" ? "text-[#2563EB]": "text-[#1F2937] hover:text-[#3B82F6] transition duration-[0.3s] ease"}`}>Overview</p>
                     <p className={`cursor-pointer pl-[2em] ${current == "Jobs" ? "text-[#2563EB]": "text-[#1F2937] hover:text-[#3B82F6] transition duration-[0.3s] ease"}`}>Jobs</p>
                     <p className={`cursor-pointer pl-[2em] ${current == "Pricing" ? "text-[#2563EB]": "text-[#1F2937] hover:text-[#3B82F6] transition duration-[0.3s] ease"}`}>Pricing</p>
                 </div>
-                <div>
+                <div className="hidden lg:block pr-10">
                     <button className="border border-[#3B82F6] text-[#3B82F6] px-[1.4em] py-[0.6em] rounded-[10px] text-[0.88rem] font-semibold mr-[1em] cursor-pointer hover:bg-blue-50 hover:text-blue-600 transition ease duration-[0.3s]">Log In</button>
                     <button className="bg-[#3B82F6] text-white px-[1.4em] py-[0.6em] rounded-[10px] text-[0.88rem] font-semibold cursor-pointer hover:bg-blue-600 transition ease duration-[0.3s]">Sign In</button>
+                </div>
+                {menuOpen ? (
+                    <IoClose size={30} className="block lg:hidden cursor-pointer mr-10" color="#3B82F6" onClick={() => {setMenuOpen(!menuOpen)}} />
+                ): (
+                    <IoMdMenu size={30} className="block lg:hidden cursor-pointer mr-10" color="#3B82F6" onClick={() => {setMenuOpen(!menuOpen)}} />
+                )}
+                <div className={`${menuOpen ? "max-h-[500px]": "max-h-0"} transition-height duration-300 ease absolute w-[100%] bg-white top-[100%] z-20 lg:hidden select-none overflow-hidden`}>
+                    <div className="py-[0.5em] pt-[1em] pl-10 cursor-pointer">Overview</div>
+                    <div className="py-[0.5em] pl-10 cursor-pointer">Jobs</div>
+                    <div className="py-[0.5em] pl-10 cursor-pointer">Pricing</div>
+                    <div className="flex px-10 py-[0.5em] pb-[2em]">
+                        <button className="border border-[#3B82F6] text-[#3B82F6] w-[50%] py-[0.6em] rounded-[10px] text-[0.88rem] font-semibold cursor-pointer hover:bg-blue-50 hover:text-blue-600 transition ease duration-[0.3s]">Log In</button>
+                        <button className="ml-[1em] bg-[#3B82F6] text-white w-[50%] py-[0.6em] rounded-[10px] text-[0.88rem] font-semibold cursor-pointer hover:bg-blue-600 transition ease duration-[0.3s]">Sign In</button>
+                    </div>
                 </div>
             </div>
         </>
