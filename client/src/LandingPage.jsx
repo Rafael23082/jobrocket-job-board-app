4f91d05a8f7674c43258e1d2851fe298d36e2e61
Rafael23082 <raf.anderson777@gmail.com>
2025-07-28 08:06:46 +0700

    Landing page update

--- client/src/LandingPage.jsx
+++ client/src/LandingPage.jsx
@@ -4,7 +4,16 @@ function LandingPage(){
   return(
     <>
       <Navbar current={"Overview"} />  
-      <div className="w-[100%] lg:h-[650px] overflow-hidden"><img src="work.jpg" className="w-full h-full object-cover object-center" /></div>
+      <div className="w-[100%] overflow-hidden bg-cover bg-center text-center relative py-[11em]" style={{backgroundImage: "url(work.jpg)", fontFamily: "'Roboto', sans-serif"}}>
+        <div className="absolute inset-0 z-0" style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}></div>
+        <p className="text-white relative z-10 text-[2.5rem] font-medium" style={{fontFamily: "'Raleway', sans-serif"}}>Work with purpose, from anywhere</p>
+        <p className="text-white relative z-10 mt-[1em] font-medium text-[1.3rem]">Find your next remote opportunity in tech, design, marketing, and more.</p>
+        <button className="px-[3.5em] py-[1.3em] bg-[#3B82F6] text-white cursor-pointer hover:bg-blue-600 transition ease duration-[0.3s] relative z-10 mt-[3em] font-semibold rounded-[5px]">BROWSE JOBS</button>
+        <div className="flex mt-[4em] justify-center relative z-10 items-center">
+          <p className="text-white font-medium text-[1.15rem]">Are you hiring remote talent?</p>
+          <button className="ml-[1em] border border-white border-[2px] text-white px-[1.4em] py-[0.6em] rounded-[10px] text-[0.88rem] font-semibold mr-[1em] cursor-pointer hover:bg-blue-50 hover:text-blue-600 transition ease duration-[0.3s]">POST A JOB</button>
+        </div>
+      </div>
     </>
   );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 function Navbar({current}){
     return(
         <>
-            <div className="flex font-medium justify-between items-center px-[3em] py-[1em]" style={{fontFamily: "'Raleway', sans-serif"}}>
-                <p className="text-[2rem] font-bold">JobRocket</p>
+            <div className="flex font-medium justify-between items-center px-[3em] py-[1em]" style={{fontFamily: "'Roboto', sans-serif"}}>
+                <p className="text-[2rem] font-bold" style={{fontFamily: "'Raleway', sans-serif"}}>JobRocket</p>
                 <div className="flex text-[0.9rem]">
                     <p className={`cursor-pointer ${current == "Overview" ? "text-[#2563EB]": "text-[#1F2937] hover:text-[#3B82F6] transition duration-[0.3s] ease"}`}>Overview</p>
-                    <p className={`cursor-pointer pl-[1em] ${current == "Jobs" ? "text-[#2563EB]": "text-[#1F2937] hover:text-[#3B82F6] transition duration-[0.3s] ease"}`}>Jobs</p>
-                    <p className={`cursor-pointer pl-[1em] ${current == "Pricing" ? "text-[#2563EB]": "text-[#1F2937] hover:text-[#3B82F6] transition duration-[0.3s] ease"}`}>Pricing</p>
+                    <p className={`cursor-pointer pl-[2em] ${current == "Jobs" ? "text-[#2563EB]": "text-[#1F2937] hover:text-[#3B82F6] transition duration-[0.3s] ease"}`}>Jobs</p>
+                    <p className={`cursor-pointer pl-[2em] ${current == "Pricing" ? "text-[#2563EB]": "text-[#1F2937] hover:text-[#3B82F6] transition duration-[0.3s] ease"}`}>Pricing</p>
                 </div>
                 <div>
-                    <button className="border border-[#3B82F6] text-[#3B82F6] px-[1.4em] py-[0.6em] rounded-[10px] text-[0.88rem] font-semibold mr-[1em]">Log In</button>
-                    <button className="bg-[#3B82F6] text-white px-[1.4em] py-[0.6em] rounded-[10px] text-[0.88rem] font-semibold">Sign In</button>
+                    <button className="border border-[#3B82F6] text-[#3B82F6] px-[1.4em] py-[0.6em] rounded-[10px] text-[0.88rem] font-semibold mr-[1em] cursor-pointer hover:bg-blue-50 hover:text-blue-600 transition ease duration-[0.3s]">Log In</button>
+                    <button className="bg-[#3B82F6] text-white px-[1.4em] py-[0.6em] rounded-[10px] text-[0.88rem] font-semibold cursor-pointer hover:bg-blue-600 transition ease duration-[0.3s]">Sign In</button>
                 </div>
             </div>
         </>
