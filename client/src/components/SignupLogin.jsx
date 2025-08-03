bad130dad8122029f1fe4c148c6667a8fcc90e1d
Rafael23082 <raf.anderson777@gmail.com>
2025-08-03 10:19:31 +0700

    validation feature for login signup

--- client/src/components/SignupLogin.jsx
+++ client/src/components/SignupLogin.jsx
@@ -3,11 +3,45 @@ import { useNavigate } from "react-router-dom";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosArrowUp } from "react-icons/io";
 import { useState } from "react";
+import { IoIosWarning } from "react-icons/io";
 
 function SignupLogin({heading, subheading, fields, button, alternative, alternative2, text, minheight, signup}){
     const [dropdownOpen, setDropdownOpen] = useState(false);
-    const [dropdownText, setDropdownText] = useState("Select your role")
+    const [dropdownText, setDropdownText] = useState("Select your role");
+    const [formValues, setFormValues] = useState(() => fields.reduce((acc, field) => {
+        acc[field.name] = "";
+        return acc;
+    }, {}))
     const navigate = useNavigate();
+    const [errors, setErrors] = useState({}); 
+
+    const handleSubmit = () => {
+        let newErrors = {}
+        fields.map((field) => {
+            if (field.name == "Role"){
+                if (dropdownText == "Select your role"){
+                    newErrors[field.name] = "Select your role."
+                }
+            }
+            if (field.name == "Email"){
+                const valid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formValues[field.name]);
+                if (!valid){
+                    newErrors[field.name] = "Invalid email format."
+                }
+            }
+            if (field.name == "Password"){
+                const length = formValues[field.name].length;
+                if (length < 8){
+                    newErrors[field.name] = "Minimum of 8 characters."   
+                }
+            }
+            if (formValues[field.name].trim() == "" && field.name != "Role"){
+                newErrors[field.name] = "This field is required."
+            }
+        })
+        setErrors(newErrors);
+    }
+
     return(
         <div className="relative min-h-[100vh] w-[100%] flex justify-center items-center">
             <div className="absolute hidden sm:block top-0 w-[100%] inset-0 bg-cover bg-center" style={{backgroundImage: "url(/blue_bg.jpg)"}}></div>
@@ -18,10 +52,10 @@ function SignupLogin({heading, subheading, fields, button, alternative, alternat
                     <p className="text-gray-600 text-[0.9rem]">{subheading}</p>
                     <button className="w-[100%] border rounded-[5px] py-[0.6em] text-[0.9rem] mt-[1.5em] cursor-pointer bg-white hover:brightness-95 transition duration-[0.3s] ease"><span className="flex items-center justify-center"><img src="/google.png" className="w-[16px] mr-[0.7em]" />Continue with Google</span></button>
                     <span className="flex items-center justify-center my-[1em]"><div className="bg-gray-300 h-[1.4px] w-[140px]"></div><p className="px-[1em] text-gray-600">or</p><div className="bg-gray-300 h-[1.4px] w-[140px]"></div></span>
-                    {fields.map((field, index) => {
-                        return field.type == "select" ? (
+                    {fields.map((field, index) => (
+                        field.type == "select" ? (
                             <div key={index}>
-                                <p className={`${index == 0 ? "pt-[0em]": "pt-[1em]"} font-bold`}>Role</p>
+                                <p className={`${index == 0 ? "pt-[0em]": "pt-[1em]"} font-bold`}>{field.name} <span className="text-red-500">*</span></p>
                                 <div className="relative mt-[0.4em] border rounded-[5px]">
                                     <span className="py-[0.3em] flex items-center justify-between px-[1em] select-none" onClick={() => {setDropdownOpen(!dropdownOpen)}}>{dropdownText} {!dropdownOpen ? <IoIosArrowDown />: <IoIosArrowUp/>}</span>
                                     <div className={`flex flex-col absolute top-[calc(100%+10px)] w-[100%] rounded-[10px] select-none overflow-hidden shadow ${dropdownOpen ? "max-h-[1000px] border duration-[0.5s]": "max-h-0 border-transparent duration-[0.1s]"} bg-white transition-all`}>
@@ -29,17 +63,27 @@ function SignupLogin({heading, subheading, fields, button, alternative, alternat
                                         <p className="px-[1em] py-[0.5em] bg-white hover:brightness-90" onClick={() => {setDropdownText("Recruiter"); setDropdownOpen(!dropdownOpen)}}>Recruiter</p>
                                     </div>
                                 </div>
+                                {errors[field.name] && (
+                                    <span className="text-red-500 text-[0.75rem] pt-[0.5em] flex items-center gap-x-[5px]"><IoIosWarning />{errors[field.name]}</span>
+                                )}
                             </div>
                         ): (
                             <div key={index}>
-                                <p className={`${index == 0 ? "pt-[0em]": "pt-[1em]"} font-bold`}>{field.name}</p>
-                                <input type={field.type} className="px-[1em] mt-[0.4em] border py-[0.3em] w-[100%] rounded-[5px]" placeholder={`${field.placeholder}`} />
+                                <p className={`${index == 0 ? "pt-[0em]": "pt-[1em]"} font-bold`}>{field.name} <span className="text-red-500">*</span></p>
+                                <input type={field.type} className="px-[1em] mt-[0.4em] border py-[0.3em] w-[100%] rounded-[5px]" placeholder={`${field.placeholder}`} value={formValues[field.name]}
+                                onChange={(e) => setFormValues((prev) => ({
+                                    ...prev, [field.name]: e.target.value
+                                }))}
+                                />
+                                {errors[field.name] && (
+                                    <span className="text-red-500 text-[0.75rem] pt-[0.5em] flex items-center gap-x-[5px]"><IoIosWarning />{errors[field.name]}</span>
+                                )}
                             </div>
                         )
-                    })}
-                    <button className="bg-[#3B82F6] mt-[2em] text-white w-[100%] py-[0.6em] rounded-[10px] text-[0.88rem] font-semibold cursor-pointer hover:bg-blue-600 transition ease duration-[0.3s]">{button}</button>
+                    ))}
+                    <button className="bg-[#3B82F6] mt-[2em] text-white w-[100%] py-[0.6em] rounded-[10px] text-[0.88rem] font-semibold cursor-pointer hover:bg-blue-600 transition ease duration-[0.3s]" onClick={handleSubmit}>{button}</button>
                     <p className="text-[0.9rem] text-gray-600 pt-[1em] text-center flex justify-center flex-wrap" onClick={() => {
-                        signup ? (navigate("/login")): (navigate("/"))
+                        signup ? (navigate("/login")): (navigate("/signup"))
                     }}>{alternative}<span className="text-blue-500 cursor-pointer hover:text-blue-700 duration-[0.3s] ease flex items-center pl-[0.5em] group">{alternative2} <FaArrowRightLong className="ml-[0.3em] group-hover:translate-x-[5px] transition-transform duration-[0.3s] ease" /></span></p>
                 </div>
                  <p className="text-[0.85rem] pr-5 sm:pr-0s sm:bottom-0 sm:relative text-gray-500 pt-[2em] md:pt-[3em]">{text}</p>
